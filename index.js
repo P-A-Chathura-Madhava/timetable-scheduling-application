f1b369d39c8f14183ed20d0ab818c55351eec0e9
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-07-02 11:42:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import envs from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const dotenv = envs.config();
+const PORT = process.env.PORT || 4000;
+
+// Default Entry Point
+app.use('/', (req, res) => {
+    res.send("Server Response");
+})
+
+// Internal Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+// app.use("/api/v1/user", authRoute);
+
+// middlewares
+// app.use(notFound);
+// app.use(errorHandler);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
