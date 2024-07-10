0fb5669bdc84649e1303943c2727c85e9900fccb
Vishwa Sandun Chandrasekara <sandunv05@gmail.com>
2024-07-10 10:32:45 +0530

    lecturer route update

--- routes/lecturerRoutes.js
+++ routes/lecturerRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { addLecturer } from '../controller/lecturerController.js';
+import { addLecturer, getALecturer, updateALecturer,deletelecturer} from '../controller/lecturerController.js';
 
 const router = express.Router()
 
 router.post("/add-Lecturer", addLecturer)
+router.get("/get-Lecturer", getALecturer)
+router.post("/update-Lecturer", updateALecturer)
+router.post("/delete-Lecturer", deletelecturer)
+
 
 
 
